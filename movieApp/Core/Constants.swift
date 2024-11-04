25b0302f46994ccae9b6456f9e3819706cf9ccda
Ichsan Indra Wahyudi <ichsanindraw.dev@gmail.com>
2024-11-04 13:47:05 +0700

    movie app

---
+++ movieApp/Core/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  movieApp
+//
+//  Created by Ichsan Indra Wahyudi on 28/10/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let APP_URL = ""
+    static let IMAGE_URL = ""
+    static let API_KEY = ""
+    static let BEARER_TOKEN = ""
+    
+}
